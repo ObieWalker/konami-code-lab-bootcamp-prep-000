5ae6cbce04a8308d3a501bf475092e4ac4e78824
Obi <obinnawalker@gmail.com>
2017-11-07 12:38:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let index = 0
 function init() {
   // Write your JavaScript code inside the init() function
 
+    document.addEventListener('keydown', function(e){
+      let keyValue = parseInt(e.which || e.detail)
+
+      if (pressedKey === code[index]){
+        index++
+
+        if (pressedKey === code.length){
+          alert("you got the konami code")
+          index = 0
+        }
+      } else {
+        
+        index = 0
+      }  
+  })
 }
+init ()
